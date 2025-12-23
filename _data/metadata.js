30ea321f1e92d9a237f99d92570f0a06c14d154e
Jason Shellen <jason@shellen.com>
2025-12-22 22:13:12 -0800

    fix: update metadata with correct site info

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "Jason's Test Blog",
+	url: "https://shellen.github.io/this-is-a-test/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "A test blog for Fridge publishing",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Jason Shellen",
+		email: "shellen@gmail.com",
+		url: "https://shellen.com/"
 	}
 }
